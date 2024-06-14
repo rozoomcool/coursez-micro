2db5a075f3bd5153e49b8556aca7b6f766ace373
rozoomcool <rozoom.um@gmail.com>
2024-06-15 02:00:42 +0300

    add eureka client

---
+++ euraka-client/src/main/kotlin/com/itabrek/eurakaclient/EurakaClientApplication.kt
@@ -0,0 +1,11 @@
+package com.itabrek.eurakaclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EurakaClientApplication
+
+fun main(args: Array<String>) {
+	runApplication<EurakaClientApplication>(*args)
+}
---
+++ euraka-client/src/test/kotlin/com/itabrek/eurakaclient/EurakaClientApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.itabrek.eurakaclient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurakaClientApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
