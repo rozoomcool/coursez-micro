d7b155e98f7af383ecb2aa6068f672ac234af732
rozoomcool <rozoom.um@gmail.com>
2024-06-16 21:00:10 +0300

    configure api gateway

--- euraka-client/src/main/kotlin/com/itabrek/eurakaclient/controller/TestController.kt
+++ euraka-client/src/main/kotlin/com/itabrek/eurakaclient/controller/TestController.kt
@@ -1,9 +1,11 @@
 package com.itabrek.eurakaclient.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("main")
 class TestController {
 
     @GetMapping("/test")
